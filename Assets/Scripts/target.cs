a05e5fe36b93bf73e3a010af0b11eff131279df6
angie <88114622+angiefn@users.noreply.github.com>
2023-04-07 15:45:43 -0700

    assigning point values

--- Assets/Scripts/target.cs
+++ Assets/Scripts/target.cs
@@ -12,6 +12,8 @@ public class target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -2;
 
+    public int pointValue;
+
 
     // Start is called before the first frame update
     void Start()
@@ -34,7 +36,7 @@ public class target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
