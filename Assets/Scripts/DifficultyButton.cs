981fffb1b3ea77ae28ab52ce06aa2dea96030a0c
angie <88114622+angiefn@users.noreply.github.com>
2023-04-12 10:33:19 -0700

    difficulty buttons

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -6,15 +6,27 @@ using UnityEngine.UI;
 public class DifficultyButton : MonoBehaviour
 {
     private Button button;
+    private GameManager gameManager;
+       
+
+
     // Start is called before the first frame update
     void Start()
     {
         button = GetComponent<Button>();
+
+        button.onClick.AddListener(SetDifficulty);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + "was clicked");
         
     }
 }
